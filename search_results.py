ecaec52ec060e8903e8f466712f696a83baade73
AbdulRahman Amr <abdulrahman.amr11@gmail.com>
2024-03-04 11:41:57 +0200

    Aadding Code

---
+++ search_results.py
@@ -0,0 +1,23 @@
+import requests, sys, webbrowser, bs4
+
+query = ' '.join(sys.argv[1:])
+
+print('Searching...' + query)    # display text while downloading the search result page
+
+req = requests.get('https://pypi.org/search/?q='+ query)
+
+req.raise_for_status()
+
+soup = bs4.BeautifulSoup(req.text,'html.parser')
+
+linkelems = soup.select('.package-snippet')
+print(len(linkelems))
+numopen = min(5, len(linkelems))
+#print(linkelems[1].get('href'))
+#webbrowser.open(linkelems[1].get('href'))
+
+for i in range(numopen):    
+    urlopen = 'https://pypi.org'+linkelems[i].get('href')
+    print('Opening ...', urlopen)
+    webbrowser.open(urlopen)
+
